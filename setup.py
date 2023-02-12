4f740ef2cd79e291a0847f3e382ac7af26770d2d
Amirreza Heydari | امیررضا حیدری <amirreza09395574353@gmail.com>
2023-02-12 16:19:07 +0330

    Add input Link,title File,number Page Crawel,end massage

--- setup.py
+++ setup.py
@@ -3,44 +3,55 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from unidecode import unidecode
-Url = "https://e118.ir/main/category/43"
+print("Please Link Category in e118.ir")
+input_url = input()
+print("Please Name File Save")
+file_name_input = input()
+print("Please Number Page Crawel")
+z_number = input()
+Url = str(input_url)
                         # a/w
-f = open("numbers.txt", "a")
+f = open(str(file_name_input), "a")
 driver = webdriver.Chrome()
 driver.get(Url)
 
 def start(newurl, newZ):
     z = newZ + 1
-    matchUrl = newurl + "/" + str(z)
-    driver.get(str(matchUrl))
-    start_range = int(1)
-    end_range = int(41)
-    for x in range(start_range, end_range):
-        print(str(x))
-        # time.sleep(2)
+    if( z <= int(z_number)):
+        matchUrl = newurl + "/" + str(z)
+        driver.get(str(matchUrl))
+        start_range = int(1)
+        end_range = int(41)
+        for x in range(start_range, end_range):
+            print(str(x))
+            # time.sleep(2)
 
-        listView = driver.find_element(By.XPATH,'/html/body/section[2]/div/div/div/div[1]/div[1]/div/div[1]/a[2]')
-        listView.click()
+            listView = driver.find_element(By.XPATH,'/html/body/section[2]/div/div/div/div[1]/div[1]/div/div[1]/a[2]')
+            listView.click()
 
-        path_numberMobile ="/html/body/section[2]/div/div/div/div[1]/div[1]/ul/li["+ str(x) +"]/a/div[2]/ul/li[2]"
-        numberMobile = driver.find_element(By.XPATH,path_numberMobile)
-        
-        path_numberPhone = "/html/body/section[2]/div/div/div/div[1]/div[1]/ul/li["+ str(x) +"]/a/div[2]/ul/li[1]"
-        numberPhone = driver.find_element(By.XPATH,path_numberPhone)
+            path_numberMobile ="/html/body/section[2]/div/div/div/div[1]/div[1]/ul/li["+ str(x) +"]/a/div[2]/ul/li[2]"
+            numberMobile = driver.find_element(By.XPATH,path_numberMobile)
+            
+            path_numberPhone = "/html/body/section[2]/div/div/div/div[1]/div[1]/ul/li["+ str(x) +"]/a/div[2]/ul/li[1]"
+            numberPhone = driver.find_element(By.XPATH,path_numberPhone)
 
-        EnMobile = unidecode(numberMobile.text)
-        EnPhone = unidecode(numberPhone.text)
-        if EnMobile != " " or ' ' or "" or '' : 
-            f.write(EnMobile + '\n')
-        else : 
-            f.write("")
-        if EnPhone != " " or ' ' or "" or '' :
-            f.write(EnPhone + '\n')
-        else : 
-            f.write("")
-    start(Url, int(z))
+            EnMobile = unidecode(numberMobile.text)
+            EnPhone = unidecode(numberPhone.text)
+            if EnMobile != " " or ' ' or "" or '' : 
+                f.write(EnMobile + '\n')
+            else : 
+                f.write("")
+            if EnPhone != " " or ' ' or "" or '' :
+                f.write(EnPhone + '\n')
+            else : 
+                f.write("")
+        start(Url, int(z))
 
-    f.close()
-    driver.close()
+    else:
+        f.close()
+        driver.close()
+        print("End Work Crawel And Save Numbers File")
+              
+        print("File name : " + str(file_name_input) )
 
 start(Url,1)
